c11beca8d83660c87fbcf697255cb3b60d084fc0
Vanessa <nva.gomo926@gmail.com>
2024-05-23 15:40:28 -0500

    incializacion

---
+++ resources/views/home.blade.php
@@ -0,0 +1,308 @@
+<!--
+
+=========================================================
+* Now UI Kit - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-kit
+* Copyright 2019 Creative Tim (http://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-kit/blob/master/LICENSE.md)
+
+* Designed by www.invisionapp.com Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Now UI Kit by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+    name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="./assets/css/now-ui-kit.css?v=1.3.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="./assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="index-page sidebar-collapse">
+  <!-- Navbar -->  
+  @include('nav')
+  <!-- End Navbar -->
+
+  <div class="wrapper">
+    <div class="page-header clear-filter" filter-color="black">
+      <div class="page-header-image" data-parallax="true"
+        style="background-image:url('./assets/img/banner-principal-fondo.jpg');">
+      </div>
+      <div class="container">
+        <div class="content-center brand">
+          <!-- <img class="n-logo" src="./assets/img/now-logo.png" alt=""> -->
+          <!-- <h1 class="h1-seo" style="color: black;">COINDESCOCOL</h1> -->
+          <h1 style="color: black;  font-size: 80px;">COINDESOCOL</h1>
+          <h4 style="color: black;">CORPORACIÓN INTEGRAL PARA EL DESARROLLO SOCIAL DE COLOMBIA Y DEL ENTORNO
+            LATINOAMERICANO</h3>
+        </div>
+      </div>
+    </div>
+    <div class="main">
+    </div>
+    <!-- End .section-navbars  -->
+    <div class="section section-tabs">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 ml-auto col-xl-10 mr-auto">
+            <p class="category">Nuestros Servicios</p>
+            <!-- Nav tabs -->
+            <div class="card">
+              <div class="card-header">
+                <ul class="nav nav-tabs" role="tablist">
+                  <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#home" role="tab">
+                      <i class="now-ui-icons objects_umbrella-13"></i> Asesoria Empresarial
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#profile" role="tab">
+                      <i class="now-ui-icons shopping_cart-simple"></i> Administración Pública
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#messages" role="tab">
+                      <i class="now-ui-icons shopping_shop"></i> Actividad Empresa
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#settings" role="tab">
+                      <i class="now-ui-icons ui-2_settings-90"></i> Grupos Profesionales
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-body">
+                <!-- Tab panes -->
+                <div class="tab-content ml-5">
+                  <div class="tab-pane active" id="home" role="tabpanel">
+                    <p>1. Planificacion estrategica y organizacional</p>
+                    <p>2. Gestion financiera</p>
+                    <p>3. Objetivos y politicas de comercializacion</p>
+                    <p>4. Planificación de la producción</p>
+                    <p>5. Politicas, practicas</p>
+                  </div>
+                  <div class="tab-pane" id="profile" role="tabpanel">
+                    <p>1. Salud</p>
+                    <p>2. Deporte</p>
+                    <p>3. Cultura</p>
+                    <p>4. Vivienda</p>
+                    <p>5. Servicios Sociales</p>
+                  </div>
+                  <div class="tab-pane" id="messages" role="tabpanel">
+                    <p>1. Actividades que se centren principalmente en el desarrollo y la properidad de las empresas y
+                      gremios</p>
+                    <p>2. Actividades de las cámaras de comercio y asociaciones similares.</p>
+                    <p>3. Actividades de representacion ante organismos publicos, relaciones</p>
+                    <p>4. Planificación de la producción</p>
+                    <p>5. Politicas, practicas</p>
+                  </div>
+                  <div class="tab-pane" id="settings" role="tabpanel">
+                    <p>1. Planificacion estrategica y organizacional</p>
+                    <p>2. Gestion financiera</p>
+                    <p>3. Objetivos y politicas de comercializacion</p>
+                    <p>4. Planificación de la producción</p>
+                    <p>5. Politicas, practicas</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!-- End Section Tabs -->
+
+
+
+    <div class="section section-nucleo-icons">
+      <div class="container">
+        <div class="row align-items-center ">
+          <div class="col-lg-7 col-md-12">
+            <h2 class="title">Capacitación en gestión de proyectos de alto impacto</h2>
+            <h5 class="description">
+              Capacitación de Lideres sociales en proyectos productivos de alto impacto social en la region del bajo
+              Cauca
+            </h5>
+            <a href="https://drive.google.com/file/d/1yk1H9k_75N0h7eqBKWxdzYhmglGWEKB5/view"
+              class="btn btn-primary btn-round btn-lg" target="_blank">Ver Documento</a>
+          </div>
+          <div class="col-lg-4 col-md-12">
+            <div class="row align-items-center">
+              <a href="https://drive.google.com/file/d/1yk1H9k_75N0h7eqBKWxdzYhmglGWEKB5/view">
+                <img src="/assets/img/ImgCapacitacion.png" style="width: 90%;" class="rounded img-raised mt-auto">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-4 col-md-12">
+            <div class="align-items-center">
+              <img src="/assets/img/ImgDonacion.png" class="rounded img-raised">
+            </div>
+          </div>
+          <div class="col-lg-7 col-md-12">
+            <h2 class="title">Realiza tu donación para ayudar a la población</h2>
+            <h5 class="description">
+              Realice sus donaciones para ayudar a comunidades víctimas de la violencia y a los más necesitados, para
+              combatir el hambre y la desigualdad, para generar desarrollo económico apoyando al campo y a nuestros
+              campesinos y para financiar proyectos productivos que beneficien al país.
+            </h5>
+            <a href="" class="btn btn-primary btn-round btn-lg" target="_blank">Aportes</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="section section-signup"
+      style="background-image: url('assets/img/bg11.jpg'); background-size: cover; background-position: top center; min-height: 700px;">
+      <div class="container">
+        <div class="card">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <div class="row justify-content-center">
+              <div class="col-md-3 m-2">
+                <video controls style="width: 100%;">
+                  <source
+                    src="assets/img/Planeación Coindesocol Adecuacion de Instalaciones unidad educativa Fusagasugá.mp4"
+                    type="video/mp4">
+                </video>
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Gestión obras de mantenimiento COINDESOCOL en Fusagasugá</h5>
+                </div>
+              </div>
+              <div class="col-3 m-2">
+                <video controls style="width: 100%;">
+                  <source
+                    src="assets/img/Gestión obras de mantenimiento COINDESOCOL_480p.mp4"
+                    type="video/mp4">
+                </video>
+                <!-- <div class="carousel-caption d-none d-md-block">
+                  <h5>Nature, United States</h5>
+                </div> -->
+              </div>
+              <div class="col-3 m-2">
+                <video controls style="width: 100%;">
+                  <source
+                    src="assets/img/Planeación Coindesocol por más cupos escolares en fusagasugá.mp4"
+                    type="video/mp4">
+                </video>
+                <!-- <div class="carousel-caption d-none d-md-block">
+                  <h5>Nature, United States</h5>
+                </div> -->
+              </div>
+            </div>       
+          </div>
+        </div>        
+      </div>
+    </div>
+  </div>
+  
+  <div class="section section-download" id="#download-section" data-background-color="black">
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="text-center col-md-12 col-lg-8">
+          <h3 class="title">Conoce nuestra Historia</h3>
+          <h5 class="description">
+            La corporación Coindesocol es una entidad sin ánimo de lucro que se fundó en el año 2020 como una organización que representa los intereses de los colombianos y latinoamericanos ante la necesidad de fomentar la articulación y el diálogo entre los gobiernos, los gremios y el sector privado, también para aportar al crecimiento del sector productivo por medio...
+          </h5>
+            <div class="col-md-12">
+              <a href="/nosotros"
+                class="btn btn-neutral btn-round btn-lg">
+                <i class="now-ui-icons arrows-1_minimal-right"></i> Conocer Mas
+              </a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+  
+  
+  <footer class="footer" data-background-color="black">
+    <div class=" container ">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              coindesocol@gmail.com
+            </a>
+          </li>
+          <li>
+            <a href="http://presentation.creative-tim.com">
+              3194089689 - 3202526741 - 3108185942
+            </a>
+          </li>
+          <li>
+            <a href="http://blog.creative-tim.com">
+              Dg 77 b # 116 – 51 Bogotá, Colombia
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright" id="copyright">
+        &copy;
+        <script>
+          document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+        </script>, Designed by
+        <a href="" target="_blank">Invision</a>. Coded by
+        <a href="" target="_blank">Creative Tim</a>.
+      </div>
+    </div>
+  </footer>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="./assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="./assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+  <script src="./assets/js/now-ui-kit.js?v=1.3.0" type="text/javascript"></script>
+  <script>
+    $(document).ready(function () {
+      // the body of this function is in assets/js/now-ui-kit.js
+      nowuiKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
+</body>
+
+</html>
---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Now UI Kit by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+    name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../assets/css/now-ui-kit.css?v=1.3.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="landing-page sidebar-collapse">
+  <!-- Navbar -->
+  @include('nav')
+  <!-- End Navbar -->
+
+  <div class="wrapper">
+    <div class="page-header page-header-small">
+      <div class="page-header-image" data-parallax="true" style="background-image: url('../assets/img/bg11.jpg');">
+      </div>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">Objeto Social Coindesocol</h1>
+          <div class="text-center">
+            <a href="" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-google-plus"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">MISIÓN</h2>
+            <h5>
+              Coordinar y gestionar recursos financieros, humanos, tecnológicos, a través de la prestación de servicios
+              y donaciones que permitan la sostenibilidad con responsabilidad social, involucrando las entidades
+              públicas y privadas en pro del desarrollo y bienestar a todas las regiones del país y de su entorno, por
+              medio de la presentación de proyectos sociales encaminados al progreso de la población en general,
+              brindando el acompañamiento jurídico, económico y académico a sus miembros que propendan por ejercer la
+              labor del objeto social de la organización, permitiendo incentivar la participación ciudadana y ejercer
+              veedurías identificando las necesidades de la población actuando en procura de generar un impacto positivo
+              en la calidad de vida de las comunidades.
+            </h5>
+          </div>
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">VISIÓN</h2>
+            <h5>
+              Consolidarnos como una organización social reconocida a nivel nacional por su capital humano en el ámbito
+              de la gestión de proyectos, de tipo ambiental, científico y tecnológico, permitiendo generar empleos y
+              oportunidades de desarrollo para el país, involucrando el sector empresarial, con alianzas estratégicas,
+              interinstitucionales con organismos nacionales e internacionales para canalizar todas las ayudas en
+              función de los objetivos Institucionales.
+            </h5>
+          </div>
+        </div>
+        <div class="separator separator-primary"></div>
+
+      </div>
+    </div>
+    <div class="section section-team text-center">
+      <div class="container">
+        <h2 class="title">CORPORACIÓN INTEGRAL PARA EL DESARROLLO SOCIAL DE COLOMBIA Y DEL ENTORNO LATINOAMERICANO</h2>
+        <div class="team">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="team-player">
+                <h4 class="title">Objeto Social</h4>
+                <p>
+                  Elaborar, presentar y ejecutar proyectos sociales y ambientales, científicos y tecnológicos, que
+                  beneficien a las comunidades y población en general; además de la capacitación y desarrollo de
+                  derechos humanos, derecho internacional humanitario, la Investigación científica, tecnológica y
+                  social; fomentar las formas de participación ciudadana, comunitaria, veedurías, audiencias públicas en
+                  los asuntos públicos, de las diversas expresiones culturales, étnicas y de la mujer, las asesorías y
+                  consultorías especializadas a las entidades Públicas de todo orden en su gestión gubernamental en
+                  todas las áreas; incentivar el desarrollo académico, intelectual y social de sus miembros.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="team-player">
+                <h4 class="title">Objetivos</h4>                
+                <div class="row">
+                  <div class="col-md-10 ml-auto mr-auto">
+                    <div class="nav-align-center">
+                      <ul class="nav nav-pills nav-pills-primary nav-pills-just-icons" role="tablist">
+                        <li class="nav-item">
+                          <a class="nav-link active" data-toggle="tab" href="#home" role="tablist">
+                            <i class="now-ui-icons arrows-1_minimal-down"></i>
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" data-toggle="tab" href="#profile" role="tablist">
+                            <i class="now-ui-icons arrows-1_minimal-down"></i>
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" data-toggle="tab" href="#messages" role="tablist">
+                            <i class="now-ui-icons arrows-1_minimal-down"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <!-- Tab panes -->
+                  <div class="tab-content gallery">
+                    <div class="tab-pane active" id="home" role="tabpanel">
+                      <div class="col-md-12 ml-auto mr-auto">
+                        <ul>
+                          <li>Prestar asesoría especializada a las entidades públicas de todo orden relacionadas con el
+                            manejo presupuestal, contabilidad pública, contratación pública, formulación y evaluación de
+                            proyectos, gerencia de proyectos, administración pública, manejo de imagen, publicidad y
+                            producción de medios de comunicación.</li>
+                          <li>Realizar investigaciones sobre temas relacionados con su objeto social.</li>
+                          <li>Formular, diseñar, evaluar y presentar proyectos a organizaciones nacionales e
+                            internacionales públicas o privadas relacionadas con el desarrollo cultural de comunidades,
+                            participación y desarrollo comunitario para su ejecución en Colombia.</li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="tab-pane" id="profile" role="tabpanel">
+                      <div class="col-md-12 ml-auto mr-auto">
+                        <ul>
+                          <li>Asesorar, realizar y ejecutar proyectos ambientales que involucren la participación comunitaria y audiencias públicas relacionados con el medio ambiente</li>
+                          <li>Asesorar, formular, presentar y ejecutar proyectos que tengan relación con el manejo de imagen en momentos de crisis, publicidad y producción de medios de comunicación.</li>
+                          <li>Suscribir convenios con entidades nacionales e internacionales para la ejecución de proyectos relacionados con el objeto de la corporación.</li>
+                          <li>Suscribir convenios y contratos con entidades públicas para el manejo de sus funciones administrativas.</li>
+                          <li>Asesorar a las entidades públicas en procesos contractuales, formulación y ejecución de proyectos relacionados con su objeto.</li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div class="tab-pane" id="messages" role="tabpanel">
+                      <div class="col-md-12 ml-auto mr-auto">
+                        <ul>
+                          <li>Adquirir bienes muebles e inmuebles para el cumplimento de su objeto y funcionamiento.</li>
+                          <li>Líneas de investigación en actividades científicas y tecnológicas: la corporación podrá suscribir, con entidades públicas y privadas  convenios especial de cooperación de qué trata la ley de ciencia y tecnología, el decreto 393 de 1991 y normas que lo adicionen, reformen o sustituyan, contratos de fomento en actividades científicas y tecnológicas, contratos de administración de proyectos,  de que trata el decreto 591 de 1991 y normas que lo adicionen, reformen o sustituyan.</li>
+                          <li>Celebrar contratos de prestación de servicios profesionales para representar a entidades públicas en procesos judiciales, así como su defensa y convenios de asociación de que trata la ley 489 de 1998, contratos de que trata el art 355 de la Constitución Nacional y normas que lo desarrollan y convenios de cooperación internacional.</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="team-player">
+
+                <h4 class="title">Valores Coorporativos</h4>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section section-contact-us text-center">
+      <div class="container">
+        <h2 class="title">Contactanos</h2>
+        <p class="description">Your project is very important to us.</p>
+        <div class="row">
+          <div class="col-lg-6 text-center col-md-8 ml-auto mr-auto">
+            <div class="input-group input-lg">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="now-ui-icons users_circle-08"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="First Name...">
+            </div>
+            <div class="input-group input-lg">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="now-ui-icons ui-1_email-85"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="Email...">
+            </div>
+            <div class="textarea-container">
+              <textarea class="form-control" name="name" rows="4" cols="80" placeholder="Type a message..."></textarea>
+            </div>
+            <div class="send-button">
+              <a href="#pablo" class="btn btn-primary btn-round btn-block btn-lg">Send Message</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer footer-default">
+      <div class=" container ">
+        <nav>
+          <ul>
+            <li>
+              <a href="https://www.creative-tim.com">
+                Creative Tim
+              </a>
+            </li>
+            <li>
+              <a href="http://presentation.creative-tim.com">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="http://blog.creative-tim.com">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div class="copyright" id="copyright">
+          &copy;
+          <script>
+            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+          </script>, Designed by
+          <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by
+          <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="../assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="../assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/now-ui-kit.js?v=1.3.0" type="text/javascript"></script>
+</body>
+
+</html>
